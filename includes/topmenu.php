def8ac9452c9bdfdd394526808a51218d7027731
alba-mu <albamu2002@gmail.com>
2025-11-18 19:32:18 +0100

    Implement login

--- includes/topmenu.php
+++ includes/topmenu.php
@@ -11,25 +11,23 @@
  * Register and Login buttons. It uses Bootstrap 5 for styling and layout.
  */
 
-    $isLogged = False;
-    $isAdmin = False;
+    $_SESSOION['isLogged'] = False;
 
-    // // Comprobar si hi ha sesió iniciada
-    if(isset($_SESSION['role'])) {
+    // Comprobar si hi ha sesió iniciada
+    if(isset($_SESSION['isLogged'])) {
         $isLogged = True;
-        // Determinar si és admin
-        if ($_SESSION['role'] === 'admin') {
-            $isAdmin = True;
+        // Determinar si és advanced
+        if ($_SESSION['role'] === 'advanced') {
+            $isAdvanced = True;
         }
     }
 ?>
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title>DAWBI-M07-Pt11</title>
+        <title>DAWBI-M07-Examen</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/main.css" rel="stylesheet">
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -38,11 +36,6 @@
     <body class="d-flex flex-column min-vh-100">
     <nav class="navbar navbar-expand-xxl navbar-dark bg-dark mb-4 ps-5 pe-5">
         <div class="container-fluid">
-
-            <a class="navbar-brand d-flex align-items-center me-5" href="https://www.proven.cat">
-                <img src="images/ip_logo_sense_lletres.png" alt="Logo" class="logo-navbar me-2">
-                <h6 class="display-6">ProvenSoft</h6>
-            </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -57,40 +50,29 @@
                             Home</a>
                     </li>
 
-                    <?php if ($isAdmin): ?>
+                    <?php if ($isLogged): ?>
                         <li class="nav-item">
-                            <a class="nav-link <?= $current_page === 'adminmenus.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>" 
-                                href="adminmenus.php">
-                                Admin Menus</a>
+                            <a class="nav-link <?= $current_page === 'profile.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>" 
+                                href="profile.php">
+                                Profile page</a>
                         </li>
+                    <?php endif; ?>
+                    
+                    <?php if ($isAdvanced): ?>
                         <li class="nav-item">
-                            <a class="nav-link <?= $current_page === 'adminusers.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>" 
-                                href="adminusers.php">
-                                Admin Users</a>
+                            <a class="nav-link <?= $current_page === 'avatarManagement.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>" 
+                                href="avatarManagement.php">
+                                Avatar Management</a>
                         </li>
                     <?php endif; ?>
 
-                    <?php if ($isLogged): ?>
-                        <li class="nav-item">
-                            <a class="nav-link <?= $current_page === 'daymenu.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>" 
-                                href="daymenu.php">
-                                Day Menu</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link <?= $current_page === 'viewmenus.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>" 
-                                href="viewmenus.php">
-                                View Menus</a>
-                        </li>
-                    <?php endif; ?>
+                    
                 </ul>
 
                 <?php if (!$isLogged): ?>
                     <ul class="navbar-nav mb-2 mb-xxl-0">
-                        <li class="nav-item me-2">
-                            <a class="btn btn-outline-light btn-md" href="register.php">Register</a>
-                        </li>
                         <li class="nav-item">
-                            <a class="btn btn-outline-light btn-md" href="login.php">Login</a>
+                            <a class="btn btn-outline-light btn-md" href="index.php">Login</a>
                         </li>
                     </ul>
                 <?php endif; ?>
@@ -99,11 +81,8 @@
                     <ul class="navbar-nav mb-2 mb-xxl-0 ms-xxl-3 align-items-end">
                         <li class="nav-item d-flex align-items-center">
                             <span class="nav-link px-2">
-                                <?= htmlspecialchars($_SESSION['name'] . " " . $_SESSION['surname']); ?>
+                                <?= htmlspecialchars($_SESSION['username'] ?? ''); ?>
                             </span>
-                        </li>
-                        <li class="nav-item">
-                            <a href="logout.php" class="btn btn-outline-light btn-md ms-2">Logout</a>
                         </li>
                     </ul>
                 <?php endif; ?>
--- index.php
+++ index.php
@@ -12,54 +12,88 @@
  * if the user is not logged in. 
  */
 
-session_start();
+require_once './fn-php/fn-users.php';
+$isLogged = False;
+$msg_error = "";
+
+if (filter_has_var(INPUT_POST, "loginsubmit")) {
+
+  $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+  $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+  // search user
+  $userinfo = searchUser($username);
+  
+  if ($username === '' || $password === '') {
+    $msg_error = "Username and password are required.";
+  } else if (count($userinfo) != 0) {  // user found
+    // Check password
+    if ($userinfo[1] === $password) {
+      // start session
+      session_start();
+      // save data in session
+      $_SESSION['role'] = $userinfo[2];
+      $_SESSION['username'] = $userinfo[0];
+
+      $isLogged = True;
+    } else {  // incorrect password
+      $msg_error = "Incorrect password";
+    }
+  } else {  // user not found
+    $msg_error = "User not found";
+  }
+} else {
+    $username =  '';
+    $password =  '';
+}
+
+
 $current_page = 'index.php';
 ?>
 
 <?php include_once "includes/topmenu.php"; ?>
 
-<main class="flex-grow-1 d-flex justify-content-center align-items-center">
-  <div class="container" style="max-width: 400px;">
+<?php if (!$isLogged) : ?>
+    <main class="flex-grow-1 d-flex justify-content-center align-items-center">
+    <div class="container" style="max-width: 400px;">
 
-    <h2 class="text-center display-4 mb-4 fw-normal">Login Form</h2>
+        <h2 class="text-center display-4 mb-4 fw-normal">Login Form</h2>
 
-    <div class="card shadow mb-4">
-      <div class="card-body">
-        <?php if ($msg_error): ?>
-          <div class="alert alert-danger pt-1 pb-1"><?php echo $msg_error; ?></div>
-        <?php endif; ?>
+        <div class="card shadow mb-4">
+        <div class="card-body">
+            <?php if ($msg_error): ?>
+            <div class="alert alert-danger pt-1 pb-1"><?php echo $msg_error; ?></div>
+            <?php endif; ?>
 
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 
-          <div class="mb-3">
-            <label for="username" class="form-label fw-bold">Email:</label>
-            <input type="text" class="form-control" id="username" placeholder="Enter username" name="username"
-              value="<?php echo $username ?? ""; ?>">
-          </div>
+            <div class="mb-3">
+                <label for="username" class="form-label fw-bold">Username:</label>
+                <input type="text" class="form-control" id="username" placeholder="Enter username" name="username"
+                value="<?php echo $username ?? ""; ?>">
+            </div>
 
-          <div class="mb-3">
-            <label for="password" class="form-label fw-bold">Password:</label>
-            <input type="password" class="form-control" id="password" placeholder="Enter password" name="password"
-              value="<?php echo $password ?? ""; ?>">
-          </div>
+            <div class="mb-3">
+                <label for="password" class="form-label fw-bold">Password:</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter password" name="password"
+                value="<?php echo $password ?? ""; ?>">
+            </div>
 
-          <div class="form-check mb-3">
-            <input type="checkbox" class="form-check-input" id="remember" name="remember">
-            <label class="form-check-label" for="remember">Remember me</label>
-          </div>
+            <button type="submit" name="loginsubmit" class="btn btn-dark w-100">Submit</button>
 
-          <button type="submit" name="loginsubmit" class="btn btn-dark w-100">Submit</button>
+            </form>
 
-          <p class="text-center mt-3 mb-0">Don't have an account? <a href="register.php">Register</a> </p>
-
-        </form>
-
-      </div>
+        </div>
+        </div>
+        
     </div>
-    
-  </div>
-</main>
-
+    </main>
+<?php endif; ?>
+<?php if($isLogged): ?>
+<div>
+    <p>You are logged in</p>
+</div>
+<?php endif; ?>
 
 <?php include_once "includes/footer.php"; ?>
 
