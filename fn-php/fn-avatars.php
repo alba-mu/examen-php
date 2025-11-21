69cefb01d31f99788bfaf84a4810aa32d3f5083f
alba-mu <albamu2002@gmail.com>
2025-11-21 17:19:15 +0100

    Implement listAvatars()

---
+++ fn-php/fn-avatars.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Returns a list of available avatar images stored inside the /avatars directory.
+ *
+ * This function scans the avatars/ folder, filters only valid image files
+ * (currently JPEG and PNG), and returns an associative array where:
+ *   - The key is the filename (e.g., "avatar1.png")
+ *   - The value is the relative filepath (e.g., "avatars/avatar1.png")
+ *
+ * Files that are not images or are unreadable are ignored.
+ *
+ * @return array<string, string> Associative array in the format ['filename' => 'filepath']
+ */
+function listAvatars(){
+  $filedir='avatars/';
+  $avatars = [];
+
+  if(file_exists($filedir) && is_dir($filedir)){
+    if($handle=opendir($filedir)){
+      while(($entry=readdir($handle))!==false){			
+        if($entry!="." && $entry !=".."){
+          $filepath = $filedir . $entry;
+          if(in_array(mime_content_type($filepath), array('image/jpeg', 'image/png'))){
+            $avatars[$entry] = $filepath;
+          }
+        }
+      }
+      closedir($handle);
+    }
+  }
+  return $avatars;
+}
