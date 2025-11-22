faf918b59df71b4b39369d6797ea4c02e3a2b6ea
alba-mu <albamu2002@gmail.com>
2025-11-22 11:15:22 +0100

    Fix responsive navbar behavior

--- includes/topmenu.php
+++ includes/topmenu.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * File: topmenu.php
  * Author: Alba Muñoz
@@ -9,34 +9,36 @@
  * It adapts its links and options based on the user's login status and role.
  */
 
-    $isLogged = False;
-    $isAdvanced = False;
+$isLogged = False;
+$isAdvanced = False;
 
-    // Comprobar si hi ha sesió iniciada
-    if(isset($_SESSION['role'])) {
-        $isLogged = True;
-        // Determinar si és advanced
-        if ($_SESSION['role'] === 'advanced') {
-            $isAdvanced = True;
-        }
+// Comprobar si hi ha sesió iniciada
+if (isset($_SESSION['role'])) {
+    $isLogged = True;
+    // Determinar si és advanced
+    if ($_SESSION['role'] === 'advanced') {
+        $isAdvanced = True;
     }
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <title>DAWBI-M07-Examen</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    </head>
+<head>
+    <title>DAWBI-M07-Examen</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <body class="d-flex flex-column min-vh-100">
-    <nav class="navbar navbar-expand-xxl navbar-dark bg-dark mb-4 ps-5 pe-5">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4 ps-5 pe-5">
         <div class="container-fluid">
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+                aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -44,30 +46,30 @@
                 <ul class="navbar-nav me-auto ms-5 mb-2 mb-lg-0 gap-3 gap-xxl-4">
 
                     <li class="nav-item">
-                        <a class="nav-link <?= $current_page === 'index.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>" 
+                        <a class="nav-link <?= $current_page === 'index.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>"
                             href="index.php">
                             Home</a>
                     </li>
 
                     <?php if ($isLogged): ?>
                         <li class="nav-item">
-                            <a class="nav-link <?= $current_page === 'profile.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>" 
+                            <a class="nav-link <?= $current_page === 'profile.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>"
                                 href="profile.php">
                                 Profile page</a>
                         </li>
                     <?php endif; ?>
-                    
+
                     <?php if ($isAdvanced): ?>
                         <li class="nav-item">
-                            <a class="nav-link <?= $current_page === 'avatarManagement.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>" 
+                            <a class="nav-link <?= $current_page === 'avatarManagement.php' ? 'active bg-secondary text-white rounded px-2' : '' ?>"
                                 href="avatarManagement.php">
                                 Avatar Management</a>
                         </li>
                     <?php endif; ?>
 
-                    
+
                 </ul>
-                
+
                 <?php if ($isLogged): ?>
                     <ul class="navbar-nav mb-2 mb-xxl-0 ms-xxl-3 align-items-end">
                         <li class="nav-item d-flex align-items-center">
